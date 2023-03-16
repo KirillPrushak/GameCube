c019d64f8fcbf042df39f99c8934bc1c05b33c05
KirillPrushak <kirillprushak@gmail.com>
2023-03-16 23:34:11 +0300

    Fix script

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,9 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class GameStateManager:MonoBehaviour
     {
+        public static GameStateManager Instance;
+        private bool _isDead = false;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(Instance.gameObject);
+                return;
+            }
+            Instance = this;
+        }
+
+        public void Die()
+        {
+            Debug.Log("Player Died");
+            _isDead = true;
+        }
         
     }
 }
