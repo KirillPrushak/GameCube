63caf5fe82b157cbdb33f01483f6c1c965f08d5f
KirillPrushak <kirillprushak@gmail.com>
2023-03-21 17:19:17 +0300

    Fix script

---
+++ Assets/Scripts/EventObjects/MovingPlatform.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public void Move()
+    {
+        gameObject.transform.position += new Vector3(Time.deltaTime * -0.5f, 0, Time.deltaTime * -0.5f);
+    }
+}
