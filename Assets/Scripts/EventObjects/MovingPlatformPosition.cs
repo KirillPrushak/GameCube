ccfa0bde0b0c0ee8836db4e81677e234c6b5e4eb
KirillPrushak <kirillprushak@gmail.com>
2023-03-22 00:56:54 +0300

    Level platform

--- Assets/Scripts/EventObjects/MovingPlatformPosition.cs
+++ Assets/Scripts/EventObjects/MovingPlatformPosition.cs
@@ -8,10 +8,12 @@ namespace DefaultNamespace.EventObjects
         [SerializeField] private Transform _pos1;
         [SerializeField] private Transform _pos2;
         [SerializeField] private float _doretion = 2f;
+        [SerializeField] private Ease _ease = Ease.Linear;
         
         public override void Move()
         {
-            transform.DOMove(_pos1.position, _doretion);
+            transform.DOMove(_pos1.position, _doretion).SetEase(_ease);
+            transform.DOMove(_pos2.position, _doretion).SetEase(_ease);
         }
     }
 }
